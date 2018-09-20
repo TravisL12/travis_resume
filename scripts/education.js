d9a47066508f5ac6793385460f85625a1a94448e
Travis Lawrence <tlawrence@tumblr.com>
2018-09-19 22:54:36 -0700

    Links n shit

--- scripts/education.js
+++ scripts/education.js
@@ -4,13 +4,13 @@ class Education {
     this.degree = info.degree;
     this.year = info.year;
     this.location = info.location;
-    this.el = document.createElement('template');
+    this.el = document.createElement("template");
   }
 
-  createHtml () {
+  createHtml() {
     return `
     <div class="education-item">
-      <p class="degree">${this.degree || ''}</p>
+      <p class="degree">${this.degree || ""}</p>
       <p class="school">${this.school}</p>
       <p class="year">${this.year}</p>
       <p class="location">${this.location}</p>
@@ -18,34 +18,8 @@ class Education {
     `;
   }
 
-  render () {
+  render() {
     this.el.innerHTML = this.createHtml();
     return this.el.content.firstElementChild;
   }
-}
-
-const education = [
-  {
-    school: 'Univ. of California at Riverside',
-    degree: 'BS Theoretical Physics',
-    year: '1999 - 2004',
-    location: 'Riverside, CA',
-  },
-  {
-    school: 'Salford University',
-    degree: 'MSc Audio Acoustics',
-    year: '2005 - 2007',
-    location: 'Manchester, England',
-  },
-  {
-    school: 'Dev Bootcamp',
-    year: 'Summer 2013',
-    location: 'San Francisco, CA',
-  },
-];
-
-const educationEl = document.getElementById('education');
-for(let i in education) {
-  const job = new Education(education[i]);
-  educationEl.appendChild(job.render());
 }
--- scripts/experience.js
+++ scripts/experience.js
@@ -5,10 +5,10 @@ class Experience {
     this.term = info.term;
     this.location = info.location;
     this.description = info.description;
-    this.el = document.createElement('template');
+    this.el = document.createElement("template");
   }
 
-  createHtml () {
+  createHtml() {
     return `
     <div class="experience">
       <p class="company">${this.name} / ${this.title}</p>
@@ -18,53 +18,8 @@ class Experience {
     `;
   }
 
-  render () {
+  render() {
     this.el.innerHTML = this.createHtml();
     return this.el.content.firstElementChild;
   }
-}
-
-const jobs = [
-  {
-    name: 'Tumblr',
-    title: 'Front End Developer',
-    term: 'November 2016 - Current',
-    location: 'San Francisco',
-    description: `
-      I worked on the commenting system (Notes & Activities team) for Tumblr's web platform.
-      During my time a complete re-write of the web client was undertaken and I
-      rebuilt the notes interface in ReactJS and Typescript with 100% test coverage using
-      Enzyme and Jest.
-    `,
-  },
-  {
-    name: 'Sincerely Inc.',
-    title: 'Full Stack Developer',
-    term: 'May 2014 - November 2016',
-    location: 'San Francisco',
-    description: `
-      I rewrote Sincerely's entire Admin system as a web client using AngularJS and Grunt.
-      During this time I began working with PHP to build out API endpoints needed for
-      the Admin client to interface with. I also rebuit the Sincerely.com web page
-      as a web client using AngularJS.
-    `,
-  },
-  {
-    name: '50Cubes',
-    title: 'Junior Developer',
-    term: 'October 2013 - April 2014',
-    location: 'San Francisco',
-    description: `
-      I used the MEAN stack to create Wardrobe.me, a social network
-      focused on fashion and clothing. Various ideas were prototyped
-      and built out for the site including quiz games that focused on clothing
-      and brands and a reward system using online coupons.
-    `,
-  },
-];
-
-const experienceEl = document.getElementById('experience');
-for(let i in jobs) {
-  const job = new Experience(jobs[i]);
-  experienceEl.appendChild(job.render());
 }
