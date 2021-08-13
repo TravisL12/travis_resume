f274d42e1904a4d0eec455712de1a472ab762b0b
Travis Lawrence <travis@instabase.com>
2021-08-13 14:33:24 -0700

    more functions!

--- scripts/application.js
+++ scripts/application.js
@@ -2,57 +2,65 @@ import data from "../data/index.js";
 import Experience from "./experience.js";
 import Education from "./education.js";
 
-const fillIn = (selector, info) => {
+const fillInfo = (selector, info) => {
   const el = document.querySelector(selector);
   el.textContent = info;
 };
 
-document.title = `${data.firstName} ${data.lastName} - Resume`;
-
-fillIn(".first", data.firstName);
-fillIn(".last", data.lastName);
-fillIn("#email", data.email);
-fillIn("#location", data.location);
-
-fillIn("#intro", data.intro);
-fillIn("#extra-info", data.extraInfo);
-fillIn("#position-title", data.positionTitle);
-
-const experienceEl = document.getElementById("experience");
-const educationEl = document.getElementById("education");
-const skillsEl = document.querySelector("#skills ul");
-const additionalSkillsEl = document.querySelector("#additional-skills ul");
-
-const { experience, education, skills, additionalSkills } = data;
-
-for (let i in experience) {
-  const item = experience[i];
-  if (!item.hide) {
-    const job = new Experience(item);
-    experienceEl.appendChild(job.render());
+const buildExperience = (experience) => {
+  const experienceEl = document.getElementById("experience");
+  for (let i in experience) {
+    const item = experience[i];
+    if (!item.hide) {
+      const job = new Experience(item);
+      experienceEl.appendChild(job.render());
+    }
   }
-}
+};
 
-for (let i in education) {
-  const item = education[i];
-  const job = new Education(item);
-  educationEl.appendChild(job.render());
-}
-
-for (let i in skills) {
-  const skill = skills[i];
-  if (!skill.hide) {
-    const item = document.createElement("li");
-    item.textContent = skill.name;
-    skillsEl.appendChild(item);
+const buildEducation = (education) => {
+  const educationEl = document.getElementById("education");
+  for (let i in education) {
+    const item = education[i];
+    const job = new Education(item);
+    educationEl.appendChild(job.render());
   }
-}
+};
 
-for (let i in additionalSkills) {
+const buildSkills = (skills) => {
+  const skillsEl = document.querySelector("#skills ul");
+  for (let i in skills) {
+    const skill = skills[i];
+    if (!skill.hide) {
+      const item = document.createElement("li");
+      item.textContent = skill.name;
+      skillsEl.appendChild(item);
+    }
+  }
+};
+
+const buildAdditionalSkills = (additionalSkills) => {
+  const additionalSkillsEl = document.querySelector("#additional-skills ul");
   const skill = additionalSkills[i];
   if (!skill.hide) {
     const item = document.createElement("li");
     item.textContent = skill.name;
     additionalSkillsEl.appendChild(item);
   }
-}
+};
+
+document.title = `${data.firstName} ${data.lastName} - Resume`;
+
+fillInfo(".first", data.firstName);
+fillInfo(".last", data.lastName);
+fillInfo("#email", data.email);
+fillInfo("#location", data.location);
+
+fillInfo("#intro", data.intro);
+fillInfo("#extra-info", data.extraInfo);
+fillInfo("#position-title", data.positionTitle);
+
+buildExperience(data.experience);
+buildEducation(data.education);
+buildSkills(data.skills);
+buildAdditionalSkills(data.additionalSkills);
